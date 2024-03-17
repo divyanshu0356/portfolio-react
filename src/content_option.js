1b275215ecf96076a88169e96ff1261947217b4c
divyanshu0356 <dk8080498@gmail.com>
2024-03-17 18:05:13 +0530

    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,187 @@
+const logotext = "Divyanshu";
+const meta = {
+    title: "Divyanshu Kumar",
+    description: "I'm Divyanshu Kumar , A final year B.Tech Student from Durgapur",
+};
+
+const introdata = {
+    title: "I’m Divyanshu Kumar",
+    animated: {
+        first: "I love to Code",
+        second: "I code cool websites",
+         third: "You Should try my work",
+         fourth:"I also do Freelancing projects."
+    },
+    description: " A developer from India",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "I Love to code full stack websites with cool features.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Logo Designer",
+        where: "Remote",
+        date: "2024",
+    },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "Jamalya",
+    //     date: "2019",
+    // },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "ALquds",
+    //     date: "2019",
+    // },
+];
+
+const skills = [{
+        name: "Java",
+        value: 90,
+    },
+    {
+        name:"HTML5",
+        value:"90",
+    },
+    {
+        name:"CSS3",
+        value:"90",
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "Oops",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "Node.js",
+        value: 85,
+    },
+    {
+        name:"Express.js",
+        value:85,
+    },{
+        name:"MySQL",
+        value:"85",
+    },
+    {
+        name:"mongoDB",
+        value:"80",
+    },
+    {
+        name:"Git & Github",
+        value:"85",
+    },
+    {
+        name:"Data Structures & Algorithms",
+        value:"90",
+    },
+    {
+        name:"Operating Systems",
+        value:"90",
+    },
+    {
+        name:"Computer Networks",
+        value:"90",
+    },
+    {
+        name:"Database Management Systems",
+        value:"90",
+    },
+    
+];
+
+const services = [
+    {
+        title: "UI & UX Design",
+        description: "I can code complete responsive websites with cool Interface",
+    },
+    {
+        title: "Web Apps",
+        description: "I can code websites from end to end ",
+    },
+    {
+        title: "Assistant for Computer Science",
+        description: "I can also provide the mentorship and assistant for the computer science students",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "A full Stack Website for reviewing hotels using the functionality of Login & SignUp",
+        link: "https://wanderlust-y13w.onrender.com/listings",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "You can play quiz on different domains to enhance your knowledge.",
+        link: "https://divyanshu0356.github.io/Quiz-App/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Music Player Using core java & Oops Concept",
+        link: "https://github.com/divyanshu0356/Music-Player",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Converter of one currency to Another ",
+        link: "https://github.com/divyanshu0356/Currency-Convertor",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "E-Commerce Website like real-time websites",
+        link: "https://divyanshu0356.github.io/E-Commerce/",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "You can Generate Strong passwords using this app.",
+        link: "https://github.com/divyanshu0356/PASSWORD-GENERATOR",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "You can check my old-work in this portfolio.",
+        link: "https://divyanshu0356.github.io/MyPortfolio/",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "divyanshukumarcs@gmail.com",
+    YOUR_FONE: "+91-6207581032",
+    description: "Please visit the services section and If you have any queries or want to connect me then leave a response. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_7g4fxgk",
+    YOUR_TEMPLATE_ID: "template_6ltt8eg",
+    YOUR_USER_ID: "Divyanshu",
+};
+
+const socialprofils = {
+    github: "https://github.com/divyanshu0356",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/divyanshukumar9869/",
+    twitter: "https://twitter.com/divyanshu_dj03",
+    leetcode:"https://leetcode.com/Divyanshu_02/",
+    GeeksforGeeks:"https://auth.geeksforgeeks.org/user/divyanshu_2002",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
